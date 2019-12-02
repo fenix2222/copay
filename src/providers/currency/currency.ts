722a8c463a51736d868f0173b52bfe842af9f0a6
Rowan de Haas <rowandehaas@outlook.com>
2019-12-02 21:38:22 +1100

    Disable non-Stratis coins

--- src/providers/currency/currency.ts
+++ src/providers/currency/currency.ts
@@ -4,12 +4,12 @@ import { availableCoins, CoinOpts } from './coin';
 import { Token, TokenOpts } from './token';
 
 export enum Coin {
-  BTC = 'btc',
-  BCH = 'bch',
-  ETH = 'eth',
-  USDC = 'usdc',
-  GUSD = 'gusd',
-  PAX = 'pax',
+  // BTC = 'btc',
+  // BCH = 'bch',
+  // ETH = 'eth',
+  // USDC = 'usdc',
+  // GUSD = 'gusd',
+  // PAX = 'pax',
   STRAT = 'strat'
 }
 
